6dcf29f4b595aa86a468769379e5e285e694326f
janmarkballados <janmarkballados@gmail.com>
2019-02-04 14:53:03 +0800

    Added values

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 <?php
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+ksort($age);
 
+foreach($age as $name => $age_value) {
+    echo "Key=" . $name . ", Value=" . $age_value;
+    echo "<br>";
+} 
 ?>
 
